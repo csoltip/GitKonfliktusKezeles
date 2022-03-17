7c96523f5e6842d19c7c1636aa40ca28fcb9e937
Péter Csolti <csolti@oktatas.local>
2022-03-17 17:39:31 +0100

    1. név inicializálása

--- src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
+++ src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
@@ -3,7 +3,7 @@ package javakonfliktuskezeles;
 public class JavaKonfliktusKezeles {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "Cs.P.";
         String nev2;
     }
     
