6256f802f50a0f89329c7ffbddb85140183e32a4
Péter Csolti <csolti@oktatas.local>
2022-03-17 17:44:09 +0100

    konfliktus kezelés: nevek inicializálva javítás

--- src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
+++ src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
@@ -3,13 +3,8 @@ package javakonfliktuskezeles;
 public class JavaKonfliktusKezeles {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         String nev1 = "Cs.P.";
-        String nev2;
-=======
-        String nev1;
         String nev2 = "X.Y.";
->>>>>>> d00561afac6a57dc372e78ede80a347a5b9eb601
     }
     
 }
